2e1a4527f3658a1a200d5e3ea916039b4cabb0e6
Andrew Archbold <pulsegrenade@gmail.com>
2018-12-23 10:02:09 -0800

    updating preview site

--- maple-syrup/static/js/app.js
+++ maple-syrup/static/js/app.js
@@ -1,3 +1,17 @@
+$.fn.flyInFromRight = function() {
+    $this = $(this);
+
+    window.setTimeout(function() {
+        $this.removeClass('fly-from-right');
+    },1000)
+
+    console.log($this);
+    console.log('really?');
+}
+
+$(function(){    
+    $('.fly-from-right').flyInFromRight();
+});
 // function getParam(name) {
 //     SCH = document.location.search;
 //     if(window['W3T'] && (W3T['MORE_ARGS'] != "")) {
