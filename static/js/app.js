48df2945f78c335aad9267c046ca5556625feea8
Andrew Archbold <pulsegrenade@gmail.com>
2020-02-27 22:54:06 -0800

    new download buttons

--- static/js/app.js
+++ static/js/app.js
@@ -655,7 +655,7 @@ $(window).on('load', function (e) {
   // try and get the position of the download buttons.
   var $downloadButtons = $('#downloadButtons');
   var $actualButtons = $('#downloadActualButtons');
-  var $buttonsOffset = 20;
+  var $buttonsOffset = 16;
 
   console.log($downloadButtons.offset().top)
 
@@ -706,13 +706,22 @@ $(window).on('load', function (e) {
     
     var resizeTimeout;
     $(window).resize(function(){
-        $actualButtons.css({
-          top: $downloadButtons.offset().top + $buttonsOffset + 'px'
+        $downloadButtons.css({
+          opacity: 1
         })
+        $actualButtons.css({
+          top: $downloadButtons.offset().top + $buttonsOffset + 'px',
+          opacity: 0
+        })
+
         clearTimeout(resizeTimeout);
-        resizeTimeout = setTimeout(function(){    
+        resizeTimeout = setTimeout(function(){   
+            $downloadButtons.css({
+              opacity: 0
+            }) 
             $actualButtons.css({
-              top: $downloadButtons.offset().top + $buttonsOffset + 'px'
+              top: $downloadButtons.offset().top + $buttonsOffset + 'px',
+              opacity: 1
             })
             if (ogWidth < 800 && $(window).width() > 800) {
               // init the parallax if the site starts at mobile and resizes to not mobile
@@ -728,7 +737,7 @@ $(window).on('load', function (e) {
             } else {
               $('.section-footer').appendTo('#ogFooterPosition');
             }
-        }, 500);
+        }, 600);
     });
 
   }
