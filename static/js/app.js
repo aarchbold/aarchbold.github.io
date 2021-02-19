6246ba1ad45739010bf86b0d0355ffb52db336b5
Andrew Archbold <pulsegrenade@gmail.com>
2021-02-18 17:22:32 -0800

    adding phone features

--- static/js/app.js
+++ static/js/app.js
@@ -490,6 +490,40 @@ if ($('body').hasClass('true-FAQ')) {
 
 }
 
+$.fn.handlePhoneQuote = function() {
+    var $context = $(this),
+        $quotes = $('.phone-features__quote--slide',$context),
+        timer = 5000,
+        currentIndex = 0;
+
+    $quotes.hide();
+    $($quotes[currentIndex]).fadeIn();
+
+    console.log(currentIndex);
+
+    setInterval(function() {
+        if (currentIndex === 2) {
+            currentIndex = 0;
+        } else {
+            currentIndex++;
+        }
+        $quotes.fadeOut();
+        setTimeout(function() {
+            $($quotes[currentIndex]).fadeIn();
+        },500)
+        
+        console.log(currentIndex);
+    },timer);
+
+    console.log($quotes);
+}
+
+$(function() {
+
+    if ($('.phone-features').length > 0) {
+        $('.phone-features').handlePhoneQuote();
+    }
+});
 function getParam(name) {
     SCH = document.location.search;
     if(window['W3T'] && (W3T['MORE_ARGS'] != "")) {
