60c83a9740262b97941470093a718f26e40235fe
Andrew Archbold <pulsegrenade@gmail.com>
2021-03-28 13:59:38 -0700

    more mobile optimization

--- static/js/app.js
+++ static/js/app.js
@@ -911,8 +911,8 @@ $(function() {
     if ($('.testimonials-container').length > 0) {
         $('.testimonial-card').slick({
             dots: true,
-            arrows: false,
-            autoplay: true,
+            arrows: true,
+            autoplay: false,
             autoplaySpeed: 6000,
         });
     }
