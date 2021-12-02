56c5a78480a58a90326c6964754eed31eae6de2f
Andrew Archbold <pulsegrenade@gmail.com>
2021-12-01 17:41:05 -0800

    adding loading screens to the waitlist signup

--- static/js/app.js
+++ static/js/app.js
@@ -712,13 +712,18 @@ $.fn.handleSignUp = function() {
         $email = $('#emailSignup'),
         $firstname = $('#firstnameSignup'),
         $lastname = $('#lastnameSignup'),
-        $joinButton = $('#joinButton');
+        $joinButton = $('#joinButton'),
+        $formInputs = $('.form-inputs'),
+        $formLoading = $('.form-loading'),
+        $formSuccess = $('.form-success');
 
     $joinButton.click(function(e) {
         e.preventDefault();
         if ($email.val() === '') {
             alert('Please enter a valid email.');
         } else {
+            $formInputs.hide();
+            $formLoading.show();
             let payload = {
                 firstname: $firstname.val(),
                 lastname: $lastname.val(),
@@ -727,6 +732,8 @@ $.fn.handleSignUp = function() {
             }
             $.post('https://zdtqrwuc01.execute-api.us-west-2.amazonaws.com/production/waitlist', JSON.stringify(payload), function() {
                 // displayError('success', 'Thank you for your interest! We will get back to you shortly.');
+                $formSuccess.show();
+                $formLoading.hide();
                 setTimeout(function() {
                     $firstname.val('');
                     $lastname.val('');
@@ -734,6 +741,8 @@ $.fn.handleSignUp = function() {
                 },500);
             })
             .fail(function(response) {
+                $formInputs.show();
+                $formSuccess.hide();
                 console.log('fail');
                 console.log(response);
                 //displayError('error', errorMsg);
@@ -751,6 +760,9 @@ $.fn.handleSignUp = function() {
     })
 
     $closeButton.click(function(e) {
+        $formInputs.show();
+        $formLoading.hide();
+        $formSuccess.hide();
         e.preventDefault();
         $('body').css({
             overflow: 'visible'
